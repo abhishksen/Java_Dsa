9a4a421024263d15c22f32899c544f5c5a2ab727
Abhishek <codmonk21@gmail.com>
2023-08-19 13:49:31 +0530

    commit

--- multidimensional_arrays/PascalsTriangle.java
+++ multidimensional_arrays/PascalsTriangle.java
@@ -17,7 +17,7 @@ class PascalsTriangle {
         for (int i = 0; i < n; i++) {
             // ith row has i+1 columns
             ans[i] = new int[i + 1];
-            // 1st last element of every row is 1
+            // 1st & last element of every row is 1
             ans[i][0] = ans[i][i] = 1;
             for (int j = 1; j < i; j++) {
                 ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
