792943421c8c147c5a5449d5759803afb53805ff
Abhishek <codmonk21@gmail.com>
2024-03-05 13:00:07 +0530

    merge sort added

--- sorting/MergeSort.java
+++ sorting/MergeSort.java
@@ -1,52 +1,55 @@
 package sorting;
 
+import java.util.ArrayList;
+
 public class MergeSort {
     static void merge(int[] arr, int l, int m, int r) {
-        int n1 = m - l + 1;
-        int n2 = r - m;
-        int L[] = new int[n1];
-        int R[] = new int[n2];
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
-        int i = 0, j = 0;
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = l;
+        int right = m + 1;
+
+        while (left <= m && right <= r) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
             } else {
-                arr[k] = R[j];
-                j++;
+                temp.add(arr[right]);
+                right++;
             }
         }
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+
+        while (left <= m) {
+            temp.add(arr[left]);
+            left++;
         }
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+        while (right <= r) {
+            temp.add(arr[right]);
+            right++;
         }
+
+        for (int i = l; i <= r; i++) {
+            arr[i] = temp.get(i - l);
+        }
+
     }
 
-    static void sort(int[] arr, int l, int r) {
-        if (l < r) {
-            int m = l + (r - l) / 2;
-            sort(arr, l, m);
-            sort(arr, m + 1, r);
-            merge(arr, l, m, r);
+    static void mergeSort(int[] arr, int l, int r) {
+        int low = l;
+        int high = r;
+        if (low >= high) {
+            return;
         }
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
     }
 
     public static void main(String[] args) {
         System.out.println("Merge sort");
         int[] arr = { 34, 89, 6, 90, 39, 20 };
         int n = arr.length;
-        sort(arr, 0, n - 1);
+        mergeSort(arr, 0, n - 1);
         for (int i : arr)
             System.out.print(i + " ");
     }
