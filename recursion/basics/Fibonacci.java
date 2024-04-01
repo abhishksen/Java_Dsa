e97556001b0188074fa683ac99547b947fd9b69c
Abhishek <codmonk21@gmail.com>
2024-04-01 16:45:32 +0530

    created fibonacci

---
+++ recursion/basics/Fibonacci.java
@@ -0,0 +1,13 @@
+package recursion.basics;
+
+public class Fibonacci {
+    static int fibo(int n) {
+        if (n < 2)
+            return n;
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibo(9));
+    }
+}
